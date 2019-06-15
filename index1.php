764f9fcd5575ceb06f2aebf9de8b9a55f686895b
gauravt1112 <33955859+gauravt1112@users.noreply.github.com>
2019-06-15 18:52:48 +0530

    index.php to index.html

---
+++ index1.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" type="image/png"  href="img/Favicon-02.png" />
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+          <link rel="stylesheet" href="css/animate.css">
+        
+        	  <script src="https://code.getmdl.io/1.1.0/material.min.js"></script>
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.0/material.indigo-pink.min.css">  
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet"> 
+        
+    </head>
+    <body>
+    
+  <a href="mailto:theimagecode.gmail.com"> <span class="mail-container">theimagecode@gmail.com</span></a> 
+    <div class="social-media-container hidden-xs">
+    	<a href="https://www.facebook.com/TheImageCode/?fref=ts"  class="icon" target="_blank"><i class="fa fa-facebook"></i></a>	<br>
+				<a href="https://www.instagram.com/theimagecode/" class="icon"  target="_blank"><i class="fa fa-instagram" style="margin-top: 15px;"></i></a>
+    </div>
+    
+      <div class="social-media-container visible-xs">
+    	<a href="https://www.facebook.com/TheImageCode/?fref=ts"  class="icon" target="_blank"><i class="fa fa-facebook"></i></a>
+				<a href="https://www.instagram.com/theimagecode/" class="icon"  target="_blank"><i class="fa fa-instagram"></i></a>
+    </div>
+    
+    	     <div class="outer-valign-container">
+<div class="inner-valign-container">
+    <div class="container-fluid adjust-mob">
+    <div class="img-wrapper wow fadeIn" data-wow-duration="5s">
+    	<img src="img/The Image Code-01.png" class="img-responsive" style="margin: 0 auto;">
+    	</div>
+    	<div class="text-wrap wow fadeInUp" data-wow-delay="2s">We are busy styling this page </div>
+    	<div class="container-fluid abc wow fadeInUp" data-wow-delay="3s">
+    	<div class="center-wrap-cover">
+    	<div class="circle-wrap">
+    	<div class="left-line">
+    		<img src="img/Line_left-01.png" class="left-line-img">
+    	</div>
+    	
+    	<div data-toggle="modal" data-target="#myModal" class="clickable">
+    		<div class="circle-outer">
+    			<div class="circle-inner">
+    			  <div class="outer-valign-container">
+<div class="inner-valign-container">
+<div style="line-height: 1;">
+    				<span class="subscribe-now-text">SUBSCRIBE NOW</span>
+    				</div>
+    				</div>
+    				</div>
+    			</div>
+    		</div>
+    		</div>
+    		<div class="right-line">
+    			<img src="img/Line_right-01.png" class="right-line-img">
+    		</div>
+    	</div>
+    	</div>
+    	</div>
+    	<div class="modal fade" id="myModal" role="dialog" >
+				  <div class="outer-valign-container">
+<div class="inner-valign-container">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content modal-con">
+        <div class="modal-header" style="border: none;">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title" style="font-family: 'halohandletterregular'; font-size:40px; text-align: center; color: black; letter-spacing: 2px;">Subscribe to us for latest updates</h4>
+        </div>
+        <div class="modal-body">
+          <form class="send-form" method="post" action="newphpmailer/submit_post_form.php" enctype="multipart/form-data">
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"><input class="mdl-textfield__input" type="text" data-validation="custom" data-validation-regexp="^([a-zA-Z0-9.@_]+)$"  data-validation-error-msg="Please enter valid email id" id="email" name="email" /> <label class="mdl-textfield__label" for="username" style="font-family: 'latobold';">Email</label> <span class="mdl-textfield__error" style="font-family: 'latobold';">Please enter valid email id.</span></div>
+    <input type="submit" name="submit" value="SUBMIT" class="send-button" style="visibility: hidden;">
+</form>
+        </div>
+    
+       <div class="ghi">
+       
+       <a class="btn btn-1">
+      <svg>
+        <rect x="0" y="0" fill="none" width="100%" height="100%"/>
+      </svg>
+     SUBMIT
+    </a>
+   </div>
+
+      </div>
+      
+    </div>
+    </div>
+    </div>
+  </div>
+    </div>
+    
+    </div>
+    </div>
+   
+       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        
+        
+       <script>
+$(document).ready(function() {
+    $(".ghi a").click(function() {
+     $(".send-button").click();
+    });
+});
+</script>
+   <script src="js/vendor/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>	
+              <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.1/jquery.form-validator.min.js"></script>
+	<script>
+	 $.validate();
+</script>
+	 
+	
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ thank-you.html
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+<link rel="stylesheet" href="css/main.css">
+
+    <meta charset="utf-8">
+    <title>Thank You</title>
+     <link rel="icon" type="image/png"  href="img/Favicon-02.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        * {
+            line-height: 1.2;
+            margin: 0;
+        }
+
+.four-heading{
+			font-size: 36px;
+}
+
+.btn{
+    margin-top: 25px;
+    padding: 10px 20px;
+    margin-bottom: 25px;
+    border: 1px solid #000;
+    background-color: transparent;
+    font-family: 'Montserrat', sans-serif;
+}
+
+
+        html {
+            color: #888;
+            display: table;
+
+          font-family: 'Montserrat', sans-serif;
+            height: 100%;
+            text-align: center;
+            width: 100%;
+        }
+
+        body {
+        	
+            display: table-cell;
+            vertical-align: middle;
+            margin: 2em auto;
+        }
+
+        h1 {
+            color: #fff;
+            font-size: 4em;
+            font-weight: 200;
+            letter-spacing: 2px;
+                font-family: 'latobold';
+        }
+        
+        
+        h2 {
+            color: #999999;
+            font-size: 2em;
+             letter-spacing: 2px;
+                    
+        }
+
+        p {
+            margin: 0 auto;
+            width: 80%;
+/*            padding-top: 20px;*/
+            padding-bottom: 30px;
+             letter-spacing: 2px;
+             color: #64C5BD;
+    font-size: 40px;
+       font-family: 'halohandletterregular';
+            
+        }
+
+.link-control{
+		color: #1CB9A8;
+		text-decoration: none;
+		letter-spacing: 2px;
+	
+}
+
+
+.link-control a{
+		color: #1CB9A8;
+		text-decoration: none;
+	
+}
+
+.link-control a:hover{
+		color: #999999;
+	
+}
+
+
+        @media only screen and (max-width: 280px) {
+
+            body, p {
+                width: 95%;
+            }
+
+            h1 {
+                font-size: 1.5em;
+                margin: 0 0 0.3em;
+            }
+        }
+        
+    </style>
+</head>
+<body>
+    <!--<p class="four-heading">Page Not Found</p>-->
+    <p>Thank You <br> We have received your request for subscription</p>
+<!--<div class="link-control">GO BACK TO <a href="index.php">HOMEPAGE </a></div>-->
+   <div>
+		<a href="index.php" style="    position: absolute;
+    margin: 0 auto;
+    margin-left: -62px;
+    margin-bottom: 25px;">
+		<div class="circle-outer">
+    			<div class="circle-inner" style="margin-top: 9px; margin-left: 11px;">
+    			  <div class="outer-valign-container">
+<div class="inner-valign-container">
+<div style="line-height: 1;">
+    				<span class="subscribe-now-text">BACK</span>
+    				</div>
+    				</div>
+    				</div>
+    			</div>
+    		</div>
+	</a>
+	
+	</div>
+</body>
+</html>
+<!-- IE needs 512+ bytes: http://blogs.msdn.com/b/ieinternals/archive/2010/08/19/http-error-pages-in-internet-explorer.aspx -->
